e8e42abf63df989a4078732dc880f55ffdad6482
gastonedel <102158957+gastonedel@users.noreply.github.com>
2022-06-02 09:43:07 -0300

    Initial commit

---
+++ Assets/CubeBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehavior : MonoBehaviour
+{
+    Quaternion startOrientation;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(Rotate90(Vector3.left));
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            StartCoroutine(Rotate90(Vector3.right));
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            StartCoroutine(Rotate90(Vector3.forward));
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine(Rotate90(Vector3.back));
+        }
+    }
+
+
+    private IEnumerator Rotate90(Vector3 axis)
+    {
+        startOrientation = transform.rotation;
+        axis = transform.InverseTransformDirection(axis);
+        float amount = 0;
+
+        while (amount <= 90)
+        {
+            yield return new WaitForEndOfFrame();
+            amount += Time.deltaTime * 90;
+            transform.rotation = startOrientation * Quaternion.AngleAxis(amount, axis);
+        }
+        transform.rotation = startOrientation * Quaternion.AngleAxis(90, axis);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TP-Bloxzorz
+ 
